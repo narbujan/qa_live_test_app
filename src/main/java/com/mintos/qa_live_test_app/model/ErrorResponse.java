5602dc4004e4afa96d0bd4ca53f6df9c78086685
janisnarbuts <janis.narbuts@mintos.com>
2025-12-29 15:33:36 +0200

    add missing class

---
+++ src/main/java/com/mintos/qa_live_test_app/model/ErrorResponse.java
@@ -0,0 +1,12 @@
+package com.mintos.qa_live_test_app.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ErrorResponse {
+	private String error;
+}
